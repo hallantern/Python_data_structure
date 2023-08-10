71b15b49e394a8041eb450b184ccefdcaaaa877b
hal_lantern <109162270+hallantern@users.noreply.github.com>
2023-08-10 11:28:23 +0800

    Update 2023_08_10.py

--- 2023_08_10.py
+++ 2023_08_10.py
@@ -1,3 +1,4 @@
+# 从数组 nums 中读取下标为 i 的数据元素值
 def value(nums, i):
     if 0 <= i <= len(nums) - 1:
         print(nums[i])
@@ -5,5 +6,7 @@ def value(nums, i):
 
 arr = [0, 5, 2, 3, 7, 1, 6]
 value(arr, 3)
+
+
 
 
