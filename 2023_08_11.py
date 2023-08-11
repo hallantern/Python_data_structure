7d09475e11a071f628bbaeff25541f1eb64abdca
hal_lantern <109162270+hallantern@users.noreply.github.com>
2023-08-11 10:42:42 +0800

    Update 2023_08_11.py

--- 2023_08_11.py
+++ 2023_08_11.py
@@ -3,7 +3,7 @@ class Solution:
         # 第 i 趟排序
         for i in range(len(arr)):
             # 从序列中前 n - i + 1 个元素的第 1 个元素开始，相邻两个元素进行比较
-            for j in range(len(arr) - i - 1):
+            for j in range(len(arr) - i - 1): # major point
                 # 相邻两个元素进行比较，如果前者大于后者，则交换位置
                 if arr[j] > arr[j + 1]:
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
@@ -12,3 +12,4 @@ class Solution:
 
     def sortArray(self, nums: List[int]) -> List[int]:
         return self.bubbleSort(nums)
+
