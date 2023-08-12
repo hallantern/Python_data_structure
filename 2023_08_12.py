02f183af181b26b01e5b9efc11bf86d63fc9b434
hal_lantern <109162270+hallantern@users.noreply.github.com>
2023-08-12 22:42:00 +0800

    Add files via upload

---
+++ 2023_08_12.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+class Solution1:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total == target:
+                return [left + 1, right + 1]
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
+        return [-1, -1]
+
